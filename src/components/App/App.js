434ccd274da96473af256ab05e23d17a17736eb0
mayacode <mmiarecka@gmail.com>
2021-02-06 20:29:38 +0100

    refactoring + added Ball

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,16 @@ import Shell from '../Shell/Shell';
 function App() {
   const positionsInitialState = [
     [
-      {left: 150, top: 75, key: 1},
-      {left: 250, top: 75, key: 2},
-      {left: 350, top: 75, key: 3},
+      {left: 150, top: 75},
+      {left: 250, top: 75},
+      {left: 350, top: 75},
     ]
   ];
 
   const [positions, setPositions] = useState(positionsInitialState);
-  const [openedShell, setOpenedShell] = useState(0);
+  const [openedShell, setOpenedShell] = useState(-1);
+  const [ballPosition, setBallPosition] = useState(-2);
+  const [gameStarted, setGameStarted] = useState(false);
 
   function check(event) {
     const shellNr = parseInt(event.currentTarget.dataset.nr);
@@ -21,75 +23,76 @@ function App() {
   }
 
   function reset() {
-    setOpenedShell(0);
-    setPositions(positionsInitialState)
+    setOpenedShell(-1);
+    setPositions(positionsInitialState);
+    setBallPosition(getRandomNumber(3));
+    setGameStarted(true);
   }
 
   const options = [
     [
-      {left: 150, top: 75, key: 1},
-      {left: 250, top: 75, key: 2},
-      {left: 350, top: 75, key: 3},
+      {left: 150, top: 75},
+      {left: 250, top: 75},
+      {left: 350, top: 75},
     ],
     [
-      {left: 150, top: 75, key: 1},
-      {left: 350, top: 75, key: 2},
-      {left: 250, top: 75, key: 3},
+      {left: 150, top: 75},
+      {left: 350, top: 75},
+      {left: 250, top: 75},
     ],
     [
-      {left: 250, top: 75, key: 1},
-      {left: 150, top: 75, key: 2},
-      {left: 350, top: 75, key: 3},
+      {left: 250, top: 75},
+      {left: 150, top: 75},
+      {left: 350, top: 75},
     ],
     [
-      {left: 250, top: 75, key: 1},
-      {left: 350, top: 75, key: 2},
-      {left: 150, top: 75, key: 3},
+      {left: 250, top: 75},
+      {left: 350, top: 75},
+      {left: 150, top: 75},
     ],
     [
-      {left: 350, top: 75, key: 1},
-      {left: 150, top: 75, key: 2},
-      {left: 250, top: 75, key: 3},
+      {left: 350, top: 75},
+      {left: 150, top: 75},
+      {left: 250, top: 75},
     ],
     [
-      {left: 350, top: 75, key: 1},
-      {left: 250, top: 75, key: 2},
-      {left: 150, top: 75, key: 3},
+      {left: 350, top: 75},
+      {left: 250, top: 75},
+      {left: 150, top: 75},
     ],
-  ]
+  ];
 
-  const options2 = [
-    { modifyLeft: (v) => v + 4, modifyTop: (v) => v + 4 },
-    { modifyLeft: (v) => v - 4, modifyTop: (v) => v - 4 },
-    { modifyLeft: (v) => v + 4, modifyTop: (v) => v - 4 },
-    { modifyLeft: (v) => v - 4, modifyTop: (v) => v + 4 },
-  ]
+  function getRandomNumber(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
-  function getChange() {
-    const number = Math.floor(Math.random() * Math.floor(6));
+  function makeChange() {
+    const number = getRandomNumber(options.length)
     return options[number]
   }
 
   useEffect(
     () => {
-      if(positions.length < 11) {
+      if(positions.length < 11 && gameStarted) {
         setTimeout(() => {
           const newPositions = [ ...positions];
-          newPositions.push(getChange());
+          newPositions.push(makeChange());
 
           setPositions(newPositions);
         }, 500)
+      } else {
+        setGameStarted(false);
       }
     },
-    [positionsInitialState]
-  )
+    [positions, gameStarted],
+  );
 
   return (
     <>
       <button onClick={reset}>run</button>
       <div style={{ position: 'relative', width: '500px', height: '200px' }}>
-        {positions[positions.length - 1].map((pos) => (
-          <Shell key={pos.key} pos={pos} check={check} openedShell={openedShell} />
+        {positions[positions.length - 1].map((pos, index) => (
+          <Shell key={index} index={index} pos={pos} check={check} openedShell={openedShell} ballPosition={ballPosition} gameStarted={gameStarted} />
         ))}
       </div>
     </>
---
+++ src/components/Shell/Ball.js
@@ -0,0 +1,20 @@
+export function Ball() {
+  const style = {
+    display: 'inline-block',
+    backgroundColor: 'red',
+    width: '30px',
+    height: '30px',
+    position: 'absolute',
+    zIndex: -10,
+    left: '9px',
+    bottom: '-9px',
+    borderRadius: '15px',
+  };
+  return (
+    <span style={style} />
+  )
+}
+
+Ball.displayName = 'Ball';
+
+export default Ball;
