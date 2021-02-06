4ef7bf469c84bd6ba465e1ced1317c0dabd22dae
mayacode <mmiarecka@gmail.com>
2021-02-06 13:39:04 +0100

    added shells

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,43 @@
+
 import { useReducer } from "react";
 
 function App() {
+  const positionsInitialState = [
+    {left: 0, top: 0},
+    {left: 0, top: 0},
+    {left: 0, top: 0},
+  ];
+
+  function reducer(state, action) {
+    switch(action.type) {
+      default:
+        return state;
+    }
+  }
+
+  const [positions, dispatch] = useReducer(reducer, positionsInitialState);
   return (
-    <div />
+    <div style={{ display: 'flex',  }}>
+      <div style={{ transform: 'rotate(45)' }}>
+        <svg viewBox="0 0 55 55" style={{ width: '55px', height: '55px', padding: '5px' }}>
+          <path d="M0,50 C0,0 50,0 50,50"></path>
+        </svg>
+      </div>
+      <div>
+        <svg viewBox="0 0 55 55" style={{ width: '55px', height: '55px', padding: '5px' }}>
+          <path d="M0,50 C0,0 50,0 50,50"></path>
+        </svg>
+      </div>
+      <div>
+        <svg viewBox="0 0 55 55" style={{ width: '55px', height: '55px', padding: '5px' }}>
+          <path d="M0,50 C0,0 50,0 50,50"></path>
+        </svg>
+      </div>
+    </div>
   );
 }
 
 App.displayName = 'App';
 
 export default App;
+
